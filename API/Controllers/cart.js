fcab8f34eef5b0a0fcb004aec685b326d3f50902
shivam0870 <shivamkrsingh4688@gmail.com>
2024-09-26 23:23:08 +0530

    added decrease logic

--- API/Controllers/cart.js
+++ API/Controllers/cart.js
@@ -101,39 +101,38 @@ export const clearCart = async(req,res) => {
 
 //Decrease qty from cart
 
-export const decreaseProductQty = async (req, res) => {
-    try {
-        const { productId,qty,  } = req.body;
-        const userId = "66ebfba190560a9e2e01c93d";
-        //getting cart by the user id because it is user specific 
-        let cart = await Cart.findOne({ userId })
-        //if cart is not there then we will create a cart else we will increase or decrease the quantitiy of cart item
-        if (!cart) {
-            cart = new Cart({ userId, items: [] })
-        }
-        const itemIndex = cart.items.findIndex((item) => item.productId.toString() === productId) //returns -1 if no item is being found 
-        const quantity = parseInt(qty); // Ensure qty is treated as a number
-        const pricePerItem = parseFloat(price); // Ensure price is a number
-        if (itemIndex > -1) {
-            const item = cart.items[itemIndex];
-            if(item.qty > qty){
-               const pricePerUnit = item.price/item.qty 
-            }
-            cart.items[itemIndex].qty += quantity;
-            cart.items[itemIndex].price += pricePerItem * quantity;
-        }
-        else {
-            cart.items.push({ productId, qty, title, price, imgSrc })
-        }
-
-        cart.items.push({ productId, title, price, qty, imgSrc });
-        await cart.save();
-        res.json({
-            message: "Item added to Cart",
-            cart
-        })
-    } catch (error) {
-        res.json(error.message)
+export const decreaseProudctQty = async (req, res) => {
+    const { productId, qty} = req.body;
+  
+    const userId = req.user;
+  
+    let cart = await Cart.findOne({ userId });
+   
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+      // return res.json({messge:'Cart not find'})
     }
-
-}
+  
+    const itemIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+  
+    if (itemIndex > -1) {
+      const item = cart.items[itemIndex]
+  
+      if(item.qty > qty){
+          const pricePerUnit = item.price/item.qty
+  
+          item.qty -= qty
+          item.price -= pricePerUnit*qty
+      }else{
+          cart.items.splice(itemIndex,1)
+      }
+  
+    } else {
+      return res.json({messge:'invalid product Id'})
+    } 
+  
+    await cart.save();
+    res.json({ message: "Items qty decreased", cart });
+  };
