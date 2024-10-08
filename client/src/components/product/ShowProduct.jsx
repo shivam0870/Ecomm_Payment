1dedb3fd36d0c9338c55e36065fdea62b6523154
shivam0870 <shivamkrsingh4688@gmail.com>
2024-10-08 18:47:48 +0530

    Show product added

--- client/src/components/product/ShowProduct.jsx
+++ client/src/components/product/ShowProduct.jsx
@@ -5,9 +5,9 @@ const ShowProduct = () => {
   const { products } = useContext(AppContext);
   return (
     <>
-    <div className="row">
+    <div className="row container">
       
-      {products?.map((product) => <div key={product._id} className='container my-2 col-md-4'>
+      {products?.map((product) => <div key={product._id} className='my-2 col-md-4'>
         <div className="card bg-dark text-light text-center" style={{ width: "18rem" }}>
           <div className='d-flex justify-content-center align-items-center p-3'>
             <img src={product.imgSrc} className="card-img-top" alt="..." style={{ width: '280px', height: '200px', borderRadius: '15', border: '2px solid blue' }} />
